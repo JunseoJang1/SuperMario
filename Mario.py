21b94ed2f5411d820466b98bf82d4e88d4e07b66
JunseoJang1 <120238433+JunseoJang1@users.noreply.github.com>
2022-12-10 14:44:08 +0900

    Add files via upload

---
+++ Mario.py
@@ -0,0 +1,55 @@
+import pygame
+
+from pygame import draw
+from pygame.color import Color
+from pygame.sprite import Sprite
+from pygame.surface import Surface
+import sys
+
+class Player(Sprite):
+    def __init__(self):
+        super().__init__()
+        self.init_image_change()
+        
+    def init_image_change(self):
+        self.sprite_image = "Mario state\walkingR.png"
+        self.sprite_width = 33.3
+        self.sprite_height = 35
+        self.sprite_sheet = pygame.image.load(self.sprite_image).convert()
+        self.sprite_columns = 3
+        self.current_frame = 0
+        
+        self.image = Surface((self.sprite_width, self.sprite_height))
+ 
+        rect = (self.sprite_width*self.current_frame, 0,
+                  self.sprite_width, self.sprite_height)
+
+        self.image.blit(self.sprite_sheet, (0,0), rect)
+        self.image.set_colorkey(Color(255, 255, 255))
+        self.rect = self.image.get_rect()
+        
+    def image_change(self, img):
+        self.sprite_image = img
+        self.sprite_width = 33.3
+        self.sprite_height = 35
+        self.sprite_sheet = pygame.image.load(self.sprite_image).convert()
+        self.sprite_columns = 3
+        self.current_frame = 0
+        
+        self.image = Surface((self.sprite_width, self.sprite_height))
+
+        rect = (self.sprite_width*self.current_frame, 0,
+                  self.sprite_width, self.sprite_height)
+
+        self.image.blit(self.sprite_sheet, (0,0), rect)
+        self.image.set_colorkey(Color(255, 255, 255))
+        
+    def update(self):
+        if self.current_frame == self.sprite_columns -1:
+            self.current_frame = 0
+        else:
+            self.current_frame += 1
+
+        rect = (self.sprite_width*self.current_frame, 0,
+                  self.sprite_width, self.sprite_height)
+        self.image.blit(self.sprite_sheet, (0,0), rect)
---
+++ question_mark.py
@@ -0,0 +1,50 @@
+import pygame
+
+from pygame import draw
+from pygame.color import Color
+from pygame.sprite import Sprite
+from pygame.surface import Surface
+
+class Question(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.init_image_change()
+    
+    def init_image_change(self):
+        self.sprite_image = "Blocks\platform-q.png"
+        self.sprite_width = 23
+        self.sprite_height = 23
+        self.sprite_sheet = pygame.image.load(self.sprite_image).convert()
+        self.current_frame = 0
+        
+        self.image = Surface((self.sprite_width, self.sprite_height))
+
+        rect = (self.sprite_width*self.current_frame, 0,
+                  self.sprite_width, self.sprite_height)
+
+        self.image.blit(self.sprite_sheet, (0,0), rect)
+        self.image.blit( self.sprite_sheet, (0, 0), rect)
+        self.image.set_colorkey(Color(255, 0, 255))
+        self.rect = self.image.get_rect()
+        
+    def image_change(self, img):
+        self.sprite_image = img
+        self.sprite_width = 23
+        self.sprite_height = 23
+        self.sprite_sheet = pygame.image.load(self.sprite_image).convert()
+        self.current_frame = 0
+        
+        self.image = Surface((self.sprite_width, self.sprite_height))
+        self.image.set_colorkey(Color(255, 0, 255))
+
+        rect = (self.sprite_width*self.current_frame, 0,
+                  self.sprite_width, self.sprite_height)
+
+        self.image.blit(self.sprite_sheet, (0,0), rect)
+        #self.rect = self.image.get_rect()
+    
+    def update(self):
+        rect = (self.sprite_width*self.current_frame, 0,
+                  self.sprite_width, self.sprite_height)
+        self.image.blit(self.sprite_sheet, (0,0), rect)
+

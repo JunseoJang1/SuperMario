ef83e370de3e36a0d0ab34d05b4e227ed3cda445
JunseoJang1 <120238433+JunseoJang1@users.noreply.github.com>
2023-02-18 16:56:36 +0900

    Update Client.py

--- 채팅 프로그램/Client.py
+++ 채팅 프로그램/Client.py
@@ -1,16 +1,34 @@
-from socket import *
+import socket
+from _thread import *
 
+HOST = '127.0.0.1'
+PORT = 9999
+LOGIN = False
 
-port = 8080
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
 
-clientSock = socket(AF_INET, SOCK_STREAM)
-clientSock.connect(('127.0.0.1', port))
+# 서버로부터 메세지를 받는 메소드
+# 스레드로 구동 시켜, 메세지를 보내는 코드와 별개로 작동하도록 처리
+def recv_data(client_socket) :
+    while True :
+        data = client_socket.recv(1024)
 
-print('접속 완료')
+        print(repr(data.decode()))
+start_new_thread(recv_data, (client_socket,))
 
+if LOGIN == False:
+    ID = input("채팅에서 사용할 아이디를 입력해주세요 : ")
+    LOGIN = True
+    
 while True:
-    recvData = clientSock.recv(1024)
-    print('상대방 :', recvData.decode('utf-8'))
+    message = input('')
+    sender = '[ ' + ID + ' ] : '
+    if message == 'quit':
+        close_data = message
+        break
 
-    sendData = input('>>>')
-    clientSock.send(sendData.encode('utf-8'))
+    client_socket.send(sender, message.encode())
+
+
+client_socket.close()
